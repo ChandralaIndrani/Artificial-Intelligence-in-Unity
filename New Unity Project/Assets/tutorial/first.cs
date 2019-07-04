ef364f888d13e4bb999de46d8784b0d9c2395fd2
ChandralaIndrani <chandralaindrani@gmail.com>
2019-07-04 13:08:17 +0530

    artificial intelligence
    artificial intelligence

---
+++ New Unity Project/Assets/tutorial/first.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first : MonoBehaviour
+{
+   public float moveSpeed=10f;
+   public float turnSpeed=50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKey(KeyCode.UpArrow)) 
+	 transform.Translate(Vector3.forward*Time.deltaTime*moveSpeed);
+	 if(Input.GetKey(KeyCode.DownArrow))
+	 transform.Translate(-Vector3.forward*moveSpeed*Time.deltaTime);
+	 if(Input.GetKey(KeyCode.LeftArrow))
+	 transform.Rotate(-Vector3.up,turnSpeed*Time.deltaTime);
+	 if(Input.GetKey(KeyCode.RightArrow))
+	 transform.Rotate(Vector3.up,turnSpeed*Time.deltaTime);
+
+    }
+}
