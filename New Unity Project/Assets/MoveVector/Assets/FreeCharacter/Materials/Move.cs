7fdb289186d3b86329d72d058005fb0857ef583e
ChandralaIndrani <chandralaindrani@gmail.com>
2019-07-05 20:50:58 +0530

    moving vector
    moving vector

---
+++ New Unity Project/Assets/MoveVector/Assets/FreeCharacter/Materials/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public Vector3 goal = new Vector3(5,0,4);
+	public float speed=0.1f;
+
+	void Start () {
+		
+	}
+	
+	void Update () 
+	{
+		this.transform.Translate(goal*speed*Time.deltaTime);
+	}
+}
