ab85c40c69741a49f434c68675afd01840da7aeb
poojareddy22 <51116814+poojareddy22@users.noreply.github.com>
2019-07-07 11:12:43 +0530

    man movement
     commit about man movement

---
+++ MoveVector/MoveVector/Assets/FreeCharacter/Materials/Move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	Vector3 goal = new Vector3(5,0,4);
+
+	void Start () {
+		this.transform.Translate(goal);
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
