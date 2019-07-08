3d6a3f55dd3a869e4ffb8e6eb8ff2e16e073a760
ChandralaIndrani <chandralaindrani@gmail.com>
2019-07-08 13:12:39 +0530

    PUSH FORWARD
    PUSH FORWARD

--- New Unity Project/Assets/ArtificialIntelligence/Section2_Moving/Scripts/MoveLocal.cs
+++ New Unity Project/Assets/ArtificialIntelligence/Section2_Moving/Scripts/MoveLocal.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class MoveLocal : MonoBehaviour {
 
 	public Transform goal;
-	public float speed = 1f;
-	float accuracy = 1.0f;
-	public float rotSpeed = 0.4f;
+	public float speed = 0.5f;
+	float accuracy=1.0f;
+
+	
 
 	// Use this for initialization
 	void Start () {
@@ -16,18 +17,11 @@ public class MoveLocal : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		Vector3 lookAtGoal = new Vector3(goal.position.x, 
-										this.transform.position.y, 
-										goal.position.z);
-		Vector3 direction = lookAtGoal - this.transform.position;
-
-		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
-												Quaternion.LookRotation(direction), 
-												Time.deltaTime*rotSpeed);
-
-		if(Vector3.Distance(transform.position,lookAtGoal) > accuracy)
-			this.transform.Translate(0,0,speed*Time.deltaTime);
-	}
+	Vector3 lookAtGoal = new Vector3(goal.position.x,this.transform.position.y,goal.position.z);
+this.transform.LookAt(lookAtGoal);
+if(Vector3.Distance(transform.position,lookAtGoal) > accuracy);
+this.transform.Translate(0,0,speed*Time.deltaTime);
+}
 	
 
 }
