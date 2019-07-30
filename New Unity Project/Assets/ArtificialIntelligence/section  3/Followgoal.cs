6bde187d181abf373a7a5518940631cb372004cf
ChandralaIndrani <chandralaindrani@gmail.com>
2019-07-30 23:19:51 +0530

    13
    13

---
+++ New Unity Project/Assets/ArtificialIntelligence/section  3/Followgoal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocal : MonoBehaviour {
+
+	public Transform goal;
+	public float speed = 2.0f;
+	float accuracy = 1.0f;
+	float rotSpeed=0.4f;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	Vector3 lookAtGoal = new Vector3(goal.position.x,this.transform.position.y,goal.position.z);
+ Vector3 direction = lookAtGoal - this.transform.position;
+ this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime*rotSpeed);
+ this.transform.Translate(0,0,speed*Time.deltaTime);
+		//Debug.DrawRay(this.transform.position,direction,Color.red);
+		//if(Vector3.Distance (transform.position,lookAtGoal) > accuracy)
+			//this.transform.Translate(0,0,speed*Time.deltaTime);
+			}
+	
+
+}
