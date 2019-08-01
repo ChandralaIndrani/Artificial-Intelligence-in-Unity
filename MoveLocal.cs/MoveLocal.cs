6b36dc7fb4e26e281aa4233c53cd615f408f5838
uday <udaybojja16@gmail.com>
2019-08-01 20:22:33 +0530

    Revert "github cleanup"
    This reverts commit 03dbf9760fcddfc8ffba46c5fd2cad59360eb371.

---
+++ MoveLocal.cs/MoveLocal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocal : MonoBehaviour {
+
+	public Transform goal;
+	float speed = 0.5f;
+	float accuracy = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 lookAtGoal = new Vector3(goal.position.x, 
+										this.transform.position.y, 
+										goal.position.z);
+		this.transform.LookAt(lookAtGoal);
+		if(Vector3.Distance(transform.position,lookAtGoal) > accuracy)
+			this.transform.Translate(0,0,speed*Time.deltaTime);
+	}
+}
