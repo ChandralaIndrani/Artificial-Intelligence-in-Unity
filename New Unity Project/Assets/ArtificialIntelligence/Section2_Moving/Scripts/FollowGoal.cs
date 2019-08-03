34cc985b047abb86f74b9673f1bc2c5b23d83d39
uday <udaybojja16@gmail.com>
2019-08-04 00:47:07 +0530

    Fixed AI Cars

---
+++ New Unity Project/Assets/ArtificialIntelligence/Section2_Moving/Scripts/FollowGoal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGoal : MonoBehaviour {
+
+	public float speed = 2.0f;
+	public float accuracy = 1.0f;
+	public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		this.transform.LookAt(goal.position);
+		Vector3 direction = goal.position - this.transform.position;
+		Debug.DrawRay(this.transform.position,direction,Color.red);
+		if(direction.magnitude > accuracy)
+			this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+	}
+}
---
+++ New Unity Project/Assets/ArtificialIntelligence/section  3/Scripts/SimpleAIScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAIScript : MonoBehaviour {
+	public Transform goal;
+	//public text readout;
+	public float acceleration = 5f;
+	public float deceleration = 5f;
+	public float minSpeed = 0.0f;
+	public float maxSpeed = 100.0f;
+	public float brakeAngle=20.0f;
+public float rotSpeed = 1.0f;
+	float speed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	Vector3 lookAtGoal = new Vector3(goal.position.x,this.transform.position.y,goal.position.z);
+	Vector3 direction = lookAtGoal - this.transform.position;
+	if(Vector3.Angle(goal.forward,this.transform.forward) > brakeAngle && speed>10)
+	{
+	//this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime*rotSpeed);
+	speed = Mathf.Clamp(speed - (deceleration*Time.deltaTime),minSpeed,maxSpeed);
+
+	}
+	else
+	{
+	speed = Mathf.Clamp(speed + (acceleration*Time.deltaTime),minSpeed,maxSpeed);
+
+	}
+
+	this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime*rotSpeed);
+	//speed = Mathf.Clamp(speed + (acceleration*Time.deltaTime),minSpeed,maxSpeed);
+	this.transform.Translate(0,0,speed);
+	//AnalougeSpeedConverter.ShowSpeed(speed,0,100);
+	//readout.text="" + speed;
+	}
+	}
