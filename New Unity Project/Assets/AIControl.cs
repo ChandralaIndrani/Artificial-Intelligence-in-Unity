624056db2e38861d8a612eb463e16753bf430fe0
ChandralaIndrani <chandralaindrani@gmail.com>
2019-08-12 19:19:55 +0530

    navmeshes
    navigation meshes

--- New Unity Project/Assets/AIControl.cs
+++ New Unity Project/Assets/AIControl.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Graph : MonoBehaviour
+public class AIControl : MonoBehaviour
 {
+public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+       agent =this.GetComponent<NavMeshAgent>(); 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
