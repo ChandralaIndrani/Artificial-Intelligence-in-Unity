6b8b7b9db3ea45278a397af1119ae555f8a44fa7
ChandralaIndrani <chandralaindrani@gmail.com>
2019-08-12 16:39:55 +0530

    wp
    wp

--- New Unity Project/Assets/ArtificialIntelligence/Section2_Moving/Scripts/FollowGoal.cs
+++ New Unity Project/Assets/ArtificialIntelligence/Section2_Moving/Scripts/FollowGoal.cs
@@ -3,22 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowGoal : MonoBehaviour {
+    Transform goal;
+	float speed = 2.0f;
+    float accuracy = 1.0f;
+	float rotSpeed = 2.0f;
+	public GameObject wpManager;
+	GameObject[] wps;
+	GameObject currentNode;
+	int currentWP = 0;
+	Graph g;
 
-	public float speed = 2.0f;
-	public float accuracy = 1.0f;
-	public Transform goal;
+ 
 
 	// Use this for initialization
 	void Start () {
-		
+	wps=wpManager.GetComponent<WPManager>().waypoints;
+	g = wpManager.GetComponent<WPManager>().graph;
+	currentNode=wps[7];
+		}
+	public void GoToHeli()
+	{
+		g.AStar(currentNode,wps[8]);
+		currentWP = 0;
+	}
+	public void GoToRuin()
+	{
+	g.AStar(currentNode , wps[0]);
+	currentWP = 0;
+	
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		this.transform.LookAt(goal.position);
-		Vector3 direction = goal.position - this.transform.position;
-		Debug.DrawRay(this.transform.position,direction,Color.red);
-		if(direction.magnitude > accuracy)
-			this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+	if(g.getPathLength() == 0 ||currentWP == g.getPathLength())
+	 return;
+	 currentNode = g.getPathPoint(currentWP);
+	 {
+	 	 currentWP++;
+
+	 }
+	 if(currentWP < g.getPathLength())
+	 {
+	 	 goal = g.getPathPoint(currentWP).transform;
+		 Vector3 LookAtGoal = new Vector3(goal.position.x , this.transform.position.y, goal.position.z);
+		 Vector3 direction = LookAtGoal - this.transform.position ;
+		 this.transform.rotation = Quaternion.Slerp(this.transform.rotation , Quaternion.LookRotation(direction) , Time.deltaTime*rotSpeed);
+	 }
 	}
 }
