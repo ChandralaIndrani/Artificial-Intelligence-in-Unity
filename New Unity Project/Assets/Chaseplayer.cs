702000e1e5e6a637f49d70e0facf6123c721fb40
ChandralaIndrani <chandralaindrani@gmail.com>
2019-08-15 15:31:52 +0530

    following a player on navmesh
    in this the zombie follows the player on navmesh and even offmesh links are generated

---
+++ New Unity Project/Assets/Chaseplayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Chaseplayer : MonoBehaviour
+{
+  GameObject Player;
+  NavMeshAgent agent;
+  // Start is called before the first frame update
+    void Start()
+    {
+	Player = GameObject.FindWithTag("Player");
+	agent = this.GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(Player.transform.position);
+    }
+}
