9d1745b20d82c60f75a67717c8402ce71de6ac60
ChandralaIndrani <chandralaindrani@gmail.com>
2019-08-20 21:18:42 +0530

    simulation of crowd
    simulation of crowd

--- New Unity Project/Assets/AIControl.cs
+++ New Unity Project/Assets/AIControl.cs
@@ -5,13 +5,18 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour
 {
+public GameObject goal;
 public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
        agent =this.GetComponent<NavMeshAgent>(); 
+	   agent.SetDestination(goal.transform.position);
     }
-
+	void Update()
+	{
+	
+	}
     // Update is called once per frame
     
 }
