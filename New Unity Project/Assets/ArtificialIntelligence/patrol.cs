09ddab3c0630b2e390c154af3713be4131c94b6b
uday <udaybojja16@gmail.com>
2019-08-30 23:21:58 +0530

    FSM Chase fix

--- New Unity Project/Assets/ArtificialIntelligence/patrol.cs
+++ New Unity Project/Assets/ArtificialIntelligence/patrol.cs
@@ -18,7 +18,7 @@ public class Patrol :NPCBaseFSM {
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		base.OnStateEnter(animator,stateInfo,layerIndex);
-		NPC = animator.gameObject;
+
 		currentWP = 2;
 	}
 
